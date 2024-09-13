6e0dd8272a07cb0299cbeec4a534b8e7b68cb417
Nathanne Isip <nathanneisip@gmail.com>
2024-09-14 03:32:47 +0800

    Type inference for array, regex, and nil.

--- src/ast/expression/TypeExpression.cpp
+++ src/ast/expression/TypeExpression.cpp
@@ -3,14 +3,20 @@
 DynamicObject TypeExpression::visit(SymbolTable& symbols) {
     DynamicObject value = this->expression->visit(symbols);
 
-    if(value.isBool())
+    if(value.isArray())
+        return std::move(DynamicObject("array"));
+    else if(value.isBool())
         return std::move(DynamicObject("bool"));
     else if(value.isString())
         return std::move(DynamicObject("string"));
     else if(value.isNumber())
         return std::move(DynamicObject("number"));
+    else if(value.isNil())
+        return std::move(DynamicObject("nil"));
     else if(value.isFunction())
         return std::move(DynamicObject("function"));
+    else if(value.isRegex())
+        return std::move(DynamicObject("regex"));
 
     throw std::runtime_error("Unknown dynamic object type.");
 }

557a451c20b6999fcf8cbca390989774f8e08a46
Nathanne Isip <nathanneisip@gmail.com>
2024-09-20 08:26:51 +0800

    Build essential checking on installation support script.

--- support/install.sh
+++ support/install.sh
@@ -3,9 +3,17 @@
 if dpkg -l | grep -q "^ii\s*git"; then
     echo "[\e[1;92m+\e[0m] Git is already installed."
 else
-    echo "[\e[1;91m-\e[0m] Git is not installed."
+    echo "[\e[1;91m-\e[0m] Git is not yet installed."
     echo "[\e[1;94m~\e[0m] Installing git..."
     apt install git -y
+fi
+
+if dpkg -l | grep -q "^ii\s*build-essential"; then
+    echo "[\e[1;92m+\e[0m] Essential build tools are already installed."
+else
+    echo "[\e[1;91m-\e[0m] Essential build tools aren't installed yet."
+    echo "[\e[1;94m~\e[0m] Installing build essentials..."
+    apt install build-essential -y
 fi
 
 git clone https://github.com/nthnn/zhivo.git --depth 1

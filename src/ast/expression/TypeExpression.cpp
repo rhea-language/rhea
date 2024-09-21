4336cd2d4993c08dc28a15f1bcef646177964710
Nathanne Isip <nathanneisip@gmail.com>
2024-09-21 08:29:22 +0800

    Updated the visit function for type expression AST class.

--- src/ast/expression/TypeExpression.cpp
+++ src/ast/expression/TypeExpression.cpp
@@ -23,24 +23,5 @@
 
 DynamicObject TypeExpression::visit(SymbolTable& symbols) {
     DynamicObject value = this->expression->visit(symbols);
-
-    if(value.isArray())
-        return std::move(DynamicObject("array"));
-    else if(value.isBool())
-        return std::move(DynamicObject("bool"));
-    else if(value.isString())
-        return std::move(DynamicObject("string"));
-    else if(value.isNumber())
-        return std::move(DynamicObject("number"));
-    else if(value.isNil())
-        return std::move(DynamicObject("nil"));
-    else if(value.isFunction())
-        return std::move(DynamicObject("function"));
-    else if(value.isRegex())
-        return std::move(DynamicObject("regex"));
-
-    throw ASTNodeException(
-        std::move(this->address),
-        "Unknown dynamic object type."
-    );
+    return DynamicObject(value.objectType());
 }

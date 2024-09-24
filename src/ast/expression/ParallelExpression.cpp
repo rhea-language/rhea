c8bf033091d963cbf64ab6c0b401671f674250e7
Nathanne Isip <nathanneisip@gmail.com>
2024-09-24 10:12:10 +0800

    Visit function implementation for AST parallel expression class.

---
+++ src/ast/expression/ParallelExpression.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of Zhivo.
+ * 
+ * Zhivo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * Zhivo is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Zhivo. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <ast/expression/ParallelExpression.hpp>
+#include <core/Runtime.hpp>
+#include <core/SymbolTable.hpp>
+
+#include <functional>
+#include <memory>
+#include <thread>
+
+DynamicObject ParallelExpression::visit(SymbolTable& symbols) {
+    std::thread task(
+        [this, expr = std::move(this->expression), sym = &symbols]() mutable {
+            expr->visit(*sym);
+        }
+    );
+
+    symbols.addParallelism(std::move(task));
+    return {};
+}

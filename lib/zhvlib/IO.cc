4c6e59e13eda89add474a201e9474f6ed2d6c728
Nathanne Isip <nathanneisip@gmail.com>
2024-10-19 12:06:57 +0800

    Moved IO native function source files to zhvlib folder.

---
+++ lib/zhvlib/IO.cc
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of Zhivo.
+ * 
+ * Zhivo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * Zhivo is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Zhivo. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "zhvlib/IO.hpp"
+
+#include <exception>
+#include <iostream>
+
+ZHIVO_FUNC(io_print) {
+    if(args.size() == 0)
+        return {};
+
+    for(size_t i = 0; i < args.size(); i++) {
+        DynamicObject arg = args.at(i);
+        std::cout << arg.toString();
+    }
+
+    return {};
+}
+
+ZHIVO_FUNC(io_read) {
+    std::string str;
+    std::getline(std::cin, str);
+
+    return DynamicObject(str);
+}
+
+ZHIVO_FUNC(io_exit) {
+    if(args.size() == 0)
+        exit(0);
+
+    DynamicObject exitCode = args.at(0);
+    if(!exitCode.isNumber())
+        throw std::runtime_error("Exit code is not a number.");
+
+    exit(static_cast<int>(exitCode.getNumber()));
+    return {};
+}

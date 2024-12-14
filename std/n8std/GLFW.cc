4020d33f918344ac4a78bb0d741ef6b59ae1b51f
Nathanne Isip <nathanneisip@gmail.com>
2024-12-14 14:28:17 +0800

    Added basic GLFW functionalities to standard library implementation.

--- std/n8std/GLFW.cc
+++ std/n8std/GLFW.cc
@@ -19,9 +19,14 @@
 #include "n8std/GLFW.hpp"
 
 #include <n8/ast/TerminativeSignal.hpp>
+#include <n8/util/RandomUtil.hpp>
+
 #include <GLFW/glfw3.h>
+#include <unordered_map>
 
 #ifndef __TERMUX__
+
+std::unordered_map<std::string, GLFWwindow*> windowMap;
 
 N8_FUNC(glfw_init) {
     return DynamicObject(glfwInit() == 1);
@@ -30,6 +35,162 @@ N8_FUNC(glfw_init) {
 N8_FUNC(glfw_terminate) {
     glfwTerminate();
     return DynamicObject();
+}
+
+N8_FUNC(glfw_createWindow) {
+    if(args.size() != 3)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 3 argument, got " +
+                std::to_string(args.size())
+        );
+
+    DynamicObject width = args.at(0),
+        height = args.at(1),
+        title = args.at(2);
+    
+    if(!width.isNumber())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting number argument for width, got " +
+                width.objectType()
+        );
+    if(!height.isNumber())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting number argument for height, got " +
+                height.objectType()
+        );
+
+    GLFWwindow* window = glfwCreateWindow(
+        static_cast<int>(width.getNumber()),
+        static_cast<int>(height.getNumber()),
+        title.toString().c_str(),
+        NULL, NULL
+    );
+    std::string uuid = N8Util::generateUuid();
+
+    if(!window)
+        return DynamicObject();
+
+    windowMap[uuid] = window;
+    return DynamicObject(uuid);
+}
+
+N8_FUNC(glfw_makeContextCurrent) {
+    if(args.size() != 1)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 1 argument, got " +
+                std::to_string(args.size())
+        );
+
+    DynamicObject uuid = args.at(0);
+    std::string uuidStr = uuid.toString();
+    if(windowMap.find(uuidStr) == windowMap.end())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Window UUID does not exist"
+        );
+
+    glfwMakeContextCurrent(windowMap[uuidStr]);
+    return DynamicObject(uuidStr);
+}
+
+N8_FUNC(glfw_shouldCloseWindow) {
+    if(args.size() != 1)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 1 argument, got " +
+                std::to_string(args.size())
+        );
+
+    DynamicObject uuid = args.at(0);
+    std::string uuidStr = uuid.toString();
+
+    if(windowMap.find(uuidStr) == windowMap.end())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Window UUID does not exist"
+        );
+
+    return DynamicObject(
+        static_cast<bool>(
+            glfwWindowShouldClose(windowMap[uuidStr])
+        )
+    );
+}
+
+N8_FUNC(glfw_clear) {
+    if(args.size() != 1)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 1 argument, got " +
+                std::to_string(args.size())
+        );
+
+    DynamicObject mask = args.at(0);
+    if(!mask.isNumber())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting number argument for mask, got " +
+                mask.objectType()
+        );
+
+    glClear(static_cast<int>(mask.getNumber()));
+    return DynamicObject();
+}
+
+N8_FUNC(glfw_swapBuffers) {
+    if(args.size() != 1)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 1 argument, got " +
+                std::to_string(args.size())
+        );
+
+    DynamicObject uuid = args.at(0);
+    std::string uuidStr = uuid.toString();
+    if(windowMap.find(uuidStr) == windowMap.end())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Window UUID does not exist"
+        );
+
+    glfwSwapBuffers(windowMap[uuidStr]);
+    return DynamicObject(uuidStr);
+}
+
+N8_FUNC(glfw_pollEvents) {
+    if(args.size() != 0)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 0 argument, got " +
+                std::to_string(args.size())
+        );
+
+    glfwPollEvents();
+    return DynamicObject();
+}
+
+N8_FUNC(glfw_destroyWindow) {
+    if(args.size() != 1)
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Expecting 1 argument, got " +
+                std::to_string(args.size())
+        );
+
+    DynamicObject uuid = args.at(0);
+    std::string uuidStr = uuid.toString();
+    if(windowMap.find(uuidStr) == windowMap.end())
+        throw TerminativeThrowSignal(
+            std::move(address),
+            "Window UUID does not exist"
+        );
+
+    glfwDestroyWindow(windowMap[uuidStr]);
+    return DynamicObject(uuidStr);
 }
 
 #endif

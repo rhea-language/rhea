4618e863ba7996d035de85affa22260441877958
Nathanne Isip <nathanneisip@gmail.com>
2024-12-20 02:49:35 +0800

    Empty statement AST definition class implementation.

---
+++ src/n8/ast/statement/EmptyStatement.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of N8.
+ * 
+ * N8 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * N8 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with N8. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <n8/ast/statement/EmptyStatement.hpp>
+
+DynamicObject EmptyStatement::visit(
+    SymbolTable& symbols
+    #ifndef _MSC_VER
+    __attribute__((unused))
+    #endif
+) {
+    return DynamicObject();
+}

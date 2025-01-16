6ceb86e9dd085654cba168be70d81c73e59badec
Nathanne Isip <nathanneisip@gmail.com>
2025-01-17 04:37:45 +0800

    BinaryExpression AST class re-implementation source file.

--- src/n8/ast/expression/BinaryExpression.cpp
+++ src/n8/ast/expression/BinaryExpression.cpp
@@ -74,273 +74,77 @@ DynamicObject BinaryExpression::visit(SymbolTable& symbols) {
     DynamicObject lValue = this->left->visit(symbols);
     DynamicObject rValue = this->right->visit(symbols);
 
-    if(lValue.isNumber() && rValue.isNumber())
-        return this->applyNumOp(lValue, rValue);
-    else if(lValue.isArray() && rValue.isArray())
-        return this->applyArrayOp(lValue, rValue);
-    else if((lValue.isString() || rValue.isString()) &&
-        !(this->op == "::" || this->op == "!:"))
-        return this->applyStringOp(lValue, rValue);
-    else if(lValue.isBool() || rValue.isBool())
-        return this->applyBoolOp(lValue, rValue);
-    else if(lValue.isRegex() || rValue.isRegex())
-        return this->applyRegexOp(lValue, rValue);
+    if(this->op == "+")
+        return lValue + rValue;
+    else if(this->op == "-")
+        return lValue - rValue;
+    else if(this->op == "/")
+        return lValue / rValue;
+    else if(this->op == "\\")
+        return rValue / lValue;
+    else if(this->op == "*")
+        return lValue * rValue;
+    else if(this->op == "%")
+        return lValue % rValue;
+    else if(this->op == "&")
+        return lValue & rValue;
+    else if(this->op == "|")
+        return lValue | rValue;
+    else if(this->op == "^")
+        return lValue ^ rValue;
+    else if(this->op == "&&")
+        return lValue && rValue;
+    else if(this->op == "||")
+        return lValue || rValue;
+    else if(this->op == "==")
+        return lValue == rValue;
+    else if(this->op == "!=")
+        return lValue != rValue;
+    else if(this->op == "<")
+        return lValue < rValue;
+    else if(this->op == ">")
+        return lValue > rValue;
+    else if(this->op == "<=")
+        return lValue <= rValue;
+    else if(this->op == ">=")
+        return lValue >= rValue;
+    else if(this->op == "<<")
+        return lValue << rValue;
+    else if(this->op == ">>")
+        return lValue >> rValue;
+    else if(this->op == "?")
+        return !lValue.isNil() ?
+            lValue : rValue;
+    else if(this->op == "::") {
+        if(lValue.isRegex() && rValue.isString())
+            return DynamicObject(std::regex_match(
+                rValue.getString(),
+                lValue.getRegex()->getRegex()
+            ));
+        else if(lValue.isString() && rValue.isRegex())
+            return DynamicObject(std::regex_match(
+                lValue.getString(),
+                rValue.getRegex()->getRegex()
+            ));
+    }
+    else if(this->op == "!:") {
+        if(lValue.isRegex() && rValue.isString())
+            return DynamicObject(!std::regex_match(
+                rValue.getString(),
+                lValue.getRegex()->getRegex()
+            ));
+        else if(lValue.isString() && rValue.isRegex())
+            return DynamicObject(!std::regex_match(
+                lValue.getString(),
+                rValue.getRegex()->getRegex()
+            ));
+    }
 
     throw ASTNodeException(
         std::move(this->address),
-        "Unsupported operation for these types."
+        "Unsupported operation for type '" + lValue.objectType() +
+            "' and '" + rValue.objectType() + "'."
     );
 
     return {};
-}
-
-DynamicObject BinaryExpression::applyNumOp(DynamicObject& lValue, DynamicObject& rValue) {
-    if(this->op == "+")
-        return DynamicObject(lValue.getNumber() + rValue.getNumber());
-    else if(this->op == "-")
-        return DynamicObject(lValue.getNumber() - rValue.getNumber());
-    else if(this->op == "*")
-        return DynamicObject(lValue.getNumber() * rValue.getNumber());
-    else if(this->op == "/")
-        return DynamicObject(lValue.getNumber() / rValue.getNumber());
-    else if(this->op == "\\")
-        return DynamicObject(rValue.getNumber() / lValue.getNumber());
-    else if(this->op == "%")
-        return DynamicObject((float) ((int) lValue.getNumber() % (int) rValue.getNumber()));
-    else if(this->op == "&")
-        return DynamicObject((float) ((int) lValue.getNumber() & (int) rValue.getNumber()));
-    else if(this->op == "|")
-        return DynamicObject((float) ((int) lValue.getNumber() | (int) rValue.getNumber()));
-    else if(this->op == ">")
-        return DynamicObject(lValue.getNumber() > rValue.getNumber());
-    else if(this->op == "<")
-        return DynamicObject(lValue.getNumber() < rValue.getNumber());
-    else if(this->op == ">=")
-        return DynamicObject(lValue.getNumber() >= rValue.getNumber());
-    else if(this->op == "<=")
-        return DynamicObject(lValue.getNumber() <= rValue.getNumber());
-    else if(this->op == "==")
-        return DynamicObject(std::fabs(lValue.getNumber() - rValue.getNumber()) <
-            std::numeric_limits<double>::epsilon());
-    else if(this->op == "!=")
-        return DynamicObject(std::fabs(lValue.getNumber() - rValue.getNumber()) >=
-            std::numeric_limits<double>::epsilon());
-    else if(this->op == "<<")
-        return DynamicObject((float) ((unsigned long) lValue.getNumber() << (unsigned long) rValue.getNumber()));
-    else if(this->op == ">>")
-        return DynamicObject((float) ((unsigned long) lValue.getNumber() >> (unsigned long) rValue.getNumber()));
-
-    throw ASTNodeException(
-        std::move(this->address),
-        "Unknown operator: " + this->address->getImage()
-    );
-}
-
-DynamicObject BinaryExpression::applyStringOp(DynamicObject& lValue, DynamicObject& rValue) {
-    if(this->op == "+")
-        return DynamicObject(lValue.toString() + rValue.toString());
-    else if(this->op == "-")
-        return DynamicObject(std::regex_replace(
-            lValue.toString(),
-            std::regex(rValue.toString()),
-            ""
-        ));
-    else if(this->op == "*") {
-        unsigned long count = 0;
-        std::string output = "";
-        std::string str;
-
-        if(lValue.isNumber() && rValue.isString()) {
-            count = (unsigned long) lValue.getNumber();
-            str = rValue.getString();
-        }
-        else if(lValue.isString() && rValue.isNumber()) {
-            count = (unsigned long) rValue.getNumber();
-            str = lValue.getString();
-        }
-
-        parsync(unsigned long i = 0; i < count; i++)
-            output += str;
-        return DynamicObject(output);
-    }
-    else if(this->op == "==")
-        return DynamicObject(lValue.getString() == rValue.getString());
-    else if(this->op == "!=")
-        return DynamicObject(lValue.getString() != rValue.getString());
-
-    throw ASTNodeException(
-        std::move(this->address),
-        "Unknown operator for string: " + this->address->getImage()
-    );
-}
-
-DynamicObject BinaryExpression::applyBoolOp(DynamicObject& lValue, DynamicObject& rValue) {
-    if(this->op == "||")
-        return DynamicObject(lValue.booleanEquivalent() || rValue.booleanEquivalent());
-    else if(this->op == "&&")
-        return DynamicObject(lValue.booleanEquivalent() && rValue.booleanEquivalent());
-    else if(this->op == "==")
-        return DynamicObject(lValue == rValue);
-    else if(this->op == "!=")
-        return DynamicObject(!(lValue == rValue));
-
-    throw ASTNodeException(
-        std::move(this->address),
-        "Unsupported operation for boolean: " + this->address->getImage()
-    );
-}
-
-DynamicObject BinaryExpression::applyRegexOp(DynamicObject& lValue, DynamicObject& rValue) {
-    if(this->op == "::") {
-        if(lValue.isRegex() && rValue.isString()) {
-            std::smatch matches;
-            auto recipientString = rValue.toString();
-
-            if(std::regex_search(
-                recipientString,
-                matches,
-                lValue.getRegex()->getRegex()
-            )) return DynamicObject(true);
-            else return DynamicObject(false);
-        }
-        else if(lValue.isString() && rValue.isRegex()) {
-            std::smatch matches;
-            auto recipientString = lValue.toString();
-
-            if(std::regex_search(
-                recipientString,
-                matches,
-                rValue.getRegex()->getRegex()
-            )) return DynamicObject(true);
-            else return DynamicObject(false);
-        }
-    }
-    else if(this->op == "!:") {
-        if(lValue.isRegex() && rValue.isString()) {
-            std::smatch matches;
-            auto recipientString = rValue.toString();
-
-            if(!std::regex_search(
-                recipientString,
-                matches,
-                lValue.getRegex()->getRegex()
-            )) return DynamicObject(true);
-            else return DynamicObject(false);
-        }
-        else if(lValue.isString() && rValue.isRegex()) {
-            std::smatch matches;
-            auto recipientString = lValue.toString();
-
-            if(!std::regex_search(
-                recipientString,
-                matches,
-                rValue.getRegex()->getRegex()
-            )) return DynamicObject(true);
-            else return DynamicObject(false);
-        }
-    }
-
-    throw ASTNodeException(
-        std::move(this->address),
-        "Unsupported operation for regular expression matching."
-    );
-}
-
-DynamicObject BinaryExpression::applyArrayOp(DynamicObject& lValue, DynamicObject& rValue) {
-    parsync(auto& object : *lValue.getArray())
-        if(!object.isNumber())
-            throw ASTNodeException(
-                std::move(this->address),
-                "Unsupported binary operation for array that contains non-numbers."
-            );
-
-    parsync(auto& object : *rValue.getArray())
-        if(!object.isNumber())
-            throw ASTNodeException(
-                std::move(this->address),
-                "Unsupported binary operation for array that contains non-numbers."
-            );
-
-    if(this->op == "+")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::add(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "-")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::sub(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "*")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::mul(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "/")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::div(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "\\")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::div(
-                N8Util::object2Vector(rValue.getArray()),
-                N8Util::object2Vector(lValue.getArray())
-            )
-        );
-    else if(this->op == "%")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::rem(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "|")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::bitwiseOr(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "&")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::bitwiseAnd(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "<<")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::shiftLeft(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == ">>")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::shiftRight(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-    else if(this->op == "^")
-        return N8Util::vector2Object(
-            N8Util::VectorMath::bitwiseXor(
-                N8Util::object2Vector(lValue.getArray()),
-                N8Util::object2Vector(rValue.getArray())
-            )
-        );
-
-    throw ASTNodeException(
-        std::move(this->address),
-        "Unsupported operation for array objects."
-    );
 }

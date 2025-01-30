40acd4ef886913a7da61e3504d6741899d9d8615
Nathanne Isip <nathanneisip@gmail.com>
2025-01-31 02:36:13 +0800

    Render.hpp header file utility implementation.

---
+++ include/n8/util/Render.hpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 - Nathanne Isip
+ * This file is part of N8.
+ * 
+ * N8 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * N8 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with N8. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef N8_UTIL_RENDER_HPP
+#define N8_UTIL_RENDER_HPP
+
+#include <string>
+
+namespace N8Util {
+
+void render(std::string text);
+void renderError(std::string text);
+
+};
+
+#endif

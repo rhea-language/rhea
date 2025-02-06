4cfcf8645d447f88c5b60af821a5256c1d56b8ef
Nathanne Isip <nathanneisip@gmail.com>
2025-02-06 09:19:46 +0800

    Singularized Windows GUI installer Python build script.

--- tools/build_exe_installer.py
+++ tools/build_exe_installer.py
@@ -48,7 +48,7 @@ files = {
 }
 
 embedded_files = "tools\\windows_installer\\embedded_files.h"
-embedded_file_map = "tools\\windows_installer\\embedded_file_map.cpp"
+embedded_file_map = "tools\\windows_installer\\embedded_file_map.hpp"
 
 if os.path.exists(embedded_files):
     os.remove(embedded_files)
@@ -90,7 +90,51 @@ OUTPUT_EXE      = os.path.join(
 )
 
 subprocess.run([
-    'g++', '-static', '-o', OUTPUT_EXE,
+    'g++', '-std=c++11', '-o', OUTPUT_EXE,
+    '-DLIBDEFLATE_DLL', '-mwindows',
+
+    'tools\\windows_installer\\installer_window.cpp',
+    'tools\\windows_installer\\installer_util.cpp',
     'tools\\windows_installer\\installer.cpp',
-    '-lshlwapi', '-ladvapi32'
+
+    '-IC:\\msys64\\mingw64\\include\\gtkmm-3.0',
+    '-IC:\\msys64\\mingw64\\lib\\gtkmm-3.0\\include',
+    '-IC:\\msys64\\mingw64\\include\\atkmm-1.6',
+    '-IC:\\msys64\\mingw64\\lib\\atkmm-1.6\\include',
+    '-IC:\\msys64\\mingw64\\include\\gdkmm-3.0',
+    '-IC:\\msys64\\mingw64\\lib\\gdkmm-3.0\\include',
+    '-IC:\\msys64\\mingw64\\include\\giomm-2.4',
+    '-IC:\\msys64\\mingw64\\lib\\giomm-2.4\\include',
+    '-IC:\\msys64\\mingw64\\include\\gtk-3.0',
+    '-IC:\\msys64\\mingw64\\include\\cairo',
+    '-IC:\\msys64\\mingw64\\include\\atk-1.0',
+    '-IC:\\msys64\\mingw64\\include\\pangomm-1.4',
+    '-IC:\\msys64\\mingw64\\lib\\pangomm-1.4\\include',
+    '-IC:\\msys64\\mingw64\\include\\glibmm-2.4',
+    '-IC:\\msys64\\mingw64\\lib\\glibmm-2.4\\include',
+    '-IC:\\msys64\\mingw64\\include\\cairomm-1.0',
+    '-IC:\\msys64\\mingw64\\lib\\cairomm-1.0\\include',
+    '-IC:\\msys64\\mingw64\\include\\sigc++-2.0',
+    '-IC:\\msys64\\mingw64\\lib\\sigc++-2.0\\include',
+    '-IC:\\msys64\\mingw64\\include\\pango-1.0',
+    '-IC:\\msys64\\mingw64\\include\\pixman-1',
+    '-IC:\\msys64\\mingw64\\include\\fribidi',
+    '-IC:\\msys64\\mingw64\\include\\harfbuzz',
+    '-IC:\\msys64\\mingw64\\include\\freetype2',
+    '-IC:\\msys64\\mingw64\\include\\gdk-pixbuf-2.0',
+    '-IC:\\msys64\\mingw64\\include\\libpng16',
+    '-IC:\\msys64\\mingw64\\include\\webp',
+    '-IC:\\msys64\\mingw64\\include\\glib-2.0',
+    '-IC:\\msys64\\mingw64\\lib\\glib-2.0\\include',
+
+    '-lgtkmm-3.0', '-latkmm-1.6', '-lgdkmm-3.0',
+    '-lgdk-3', '-lz', '-lgdi32', '-limm32', '-lshell32',
+    '-lole32', '-luuid', '-lwinmm', '-ldwmapi',
+    '-lsetupapi', '-lhid', '-lwinspool', '-lcomctl32',
+    '-lcomdlg32', '-lcairo-gobject', '-latk-1.0',
+    '-lpangomm-1.4', '-lglibmm-2.4', '-lcairomm-1.0',
+    '-lsigc-2.0', '-lpangocairo-1.0', '-lpangowin32-1.0',
+    '-lpango-1.0', '-lcairo', '-lharfbuzz',
+    '-lgdk_pixbuf-2.0', '-lgio-2.0', '-lgobject-2.0',
+    '-lglib-2.0', '-lintl', '-lgiomm-2.4'
 ])

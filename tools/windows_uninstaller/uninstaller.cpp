96bb72ccc5cc5e986028f6801433a2b1c26570f5
Nathanne Isip <nathanneisip@gmail.com>
2025-02-08 00:48:46 +0800

    Initial Windows GUI uninstaller source file.

--- tools/windows_uninstaller/uninstaller.cpp
+++ tools/windows_uninstaller/uninstaller.cpp
@@ -1,49 +1,16 @@
-/*
- * Copyright (c) 2025 - Nathanne Isip
- * This file is part of N8.
- * 
- * N8 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- * 
- * N8 is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with N8. If not, see <https://www.gnu.org/licenses/>.
- */
-
 #include <fstream>
-#include <iostream>
+#include <iomanip>
 #include <string>
-#include <vector>
+#include <thread>
+#include <mutex>
+#include <queue>
 
+#include <gtkmm.h>
+#include <windows.h>
 #include <shlwapi.h>
 #include <shlobj.h>
-#include <windows.h>
 
-enum ConsoleColor {
-    Blue = 1,
-    Green = 2,
-    Aqua = 3,
-    Red = 4,
-    Purple = 5,
-    Yellow = 6,
-    White = 7
-};
-
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-void SetColor(ConsoleColor color) {
-    SetConsoleTextAttribute(hConsole, color);
-}
-
-void ResetColor() {
-    SetConsoleTextAttribute(hConsole, White);
-}
+namespace N8Uninstaller {
 
 bool IsElevated() {
     BOOL fRet = FALSE;
@@ -61,41 +28,6 @@ bool IsElevated() {
         CloseHandle(hToken);
 
     return fRet;
-}
-
-bool DeleteDirectory(const std::wstring& path) {
-    WIN32_FIND_DATAW findData;
-    HANDLE hFind;
-
-    std::wstring searchPattern = path + L"\\*.*";
-    hFind = FindFirstFileW(searchPattern.c_str(), &findData);
-
-    if(hFind == INVALID_HANDLE_VALUE)
-        return false;
-
-    do {
-        if(wcscmp(findData.cFileName, L".") == 0 || 
-            wcscmp(findData.cFileName, L"..") == 0
-        ) continue;
-
-        std::wstring filePath = path + L"\\" + findData.cFileName;
-        if(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-            if(!DeleteDirectory(filePath)) {
-                FindClose(hFind);
-                return false;
-            }
-        }
-        else {
-            if(!DeleteFileW(filePath.c_str())) {
-                FindClose(hFind);
-                return false;
-            }
-        }
-    }
-    while(FindNextFileW(hFind, &findData));
-
-    FindClose(hFind);
-    return RemoveDirectoryW(path.c_str());
 }
 
 bool SetEnvironmentVariablePersistent(const std::wstring& name, const std::wstring& value, bool systemWide) {
@@ -131,6 +63,41 @@ bool SetEnvironmentVariablePersistent(const std::wstring& name, const std::wstri
     );
 
     return true;
+}
+
+bool DeleteDirectory(const std::wstring& path) {
+    WIN32_FIND_DATAW findData;
+    HANDLE hFind;
+
+    std::wstring searchPattern = path + L"\\*.*";
+    hFind = FindFirstFileW(searchPattern.c_str(), &findData);
+
+    if(hFind == INVALID_HANDLE_VALUE)
+        return false;
+
+    do {
+        if(wcscmp(findData.cFileName, L".") == 0 || 
+            wcscmp(findData.cFileName, L"..") == 0
+        ) continue;
+
+        std::wstring filePath = path + L"\\" + findData.cFileName;
+        if(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            if(!DeleteDirectory(filePath)) {
+                FindClose(hFind);
+                return false;
+            }
+        }
+        else {
+            if(!DeleteFileW(filePath.c_str())) {
+                FindClose(hFind);
+                return false;
+            }
+        }
+    }
+    while(FindNextFileW(hFind, &findData));
+
+    FindClose(hFind);
+    return RemoveDirectoryW(path.c_str());
 }
 
 bool RemoveEnvironmentVariablePersistent(const std::wstring& name, bool systemWide) {
@@ -218,192 +185,354 @@ bool RemoveUninstallEntry() {
     return status == ERROR_SUCCESS || status == ERROR_FILE_NOT_FOUND;
 }
 
-int main() {
-    if(!IsElevated()) {
-        wchar_t path[MAX_PATH];
+}
 
-        GetModuleFileNameW(NULL, path, MAX_PATH);
-        ShellExecuteW(NULL, L"runas", path, NULL, NULL, SW_SHOWNORMAL);
+class UninstallWindow : public Gtk::Window {
+public:
+    UninstallWindow();
+    virtual ~UninstallWindow();
 
-        return 0;
-    }
+protected:
+    Gtk::Box mainBox{Gtk::ORIENTATION_VERTICAL};
+    Gtk::ScrolledWindow logScroll;
+    Gtk::TextView logView;
+    Gtk::ProgressBar progressBar;
+    Gtk::Label statusLabel;
+    Gtk::Button closeButton{"Close"};
 
-    SetColor(Aqua);
-    std::wcout << L"=== n8lang Uninstaller ===" << std::endl;
-    ResetColor();
+    Glib::RefPtr<Gtk::TextBuffer> logBuffer;
 
-    std::wstring binPath = GetInstallBase(true);
-    std::wstring docPath = GetInstallBase(false);
+    std::mutex logMutex;
+    std::thread workerThread;
 
+    void append_log(const std::string& message);
+    void update_progress(double fraction);
+    void update_status(const std::string& text);
+    void on_close_clicked();
+    void perform_uninstall();
+    void create_selfdestruct_bat();
+};
+
+UninstallWindow::UninstallWindow() {
+    this->set_title("N8 Language Uninstaller");
+    this->set_position(Gtk::WIN_POS_CENTER);
+    this->set_default_size(760, 380);
+    this->set_resizable(false);
+
+    auto css = Gtk::CssProvider::create();
+    css->load_from_data(R"(
+        * {
+            font-family: -apple-system, 'Helvetica', 'Segoe UI', sans-serif;
+        }
+
+        GtkTextView {
+            font-family: Consolas;
+        }
+
+        .button {
+            padding: 8px 20px;
+            margin: 8px 0;
+            font-weight: 500;
+            transition: all 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        }
+
+        .log-view {
+            border: 1px solid #909090;
+        }
+
+        .card {
+            box-shadow: 0 8px 24px rgba(0,0,0,0.2);
+            border: 1px solid #909090;
+            padding: 24px;
+            margin: 20px;
+        }
+    )");
+
+    this->get_style_context()->add_provider_for_screen(
+        Gdk::Screen::get_default(),
+        css,
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
+    Pango::FontDescription font;
+    font.set_family("Consolas");
+
+    this->mainBox.set_border_width(24);
+    this->mainBox.set_spacing(16);
+    this->mainBox.get_style_context()->add_class("card");
+    this->add(this->mainBox);
+
+    auto header = Gtk::make_managed<Gtk::Label>();
+    header->set_markup("<span size='20pt' weight='bold'>N8 Language Uninstaller</span>");
+    header->set_halign(Gtk::ALIGN_START);
+    this->mainBox.pack_start(*header, Gtk::PACK_SHRINK);
+
+    this->logBuffer = Gtk::TextBuffer::create();
+    this->logView.set_buffer(logBuffer);
+    this->logView.set_editable(false);
+    this->logView.set_wrap_mode(Gtk::WRAP_WORD);
+    this->logView.override_font(font);
+    
+    this->logScroll.add(this->logView);
+    this->logScroll.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC);
+    this->logScroll.set_vexpand(true);
+    this->logScroll.set_hexpand(true);
+    this->logScroll.get_style_context()->add_class("log-view");
+    this->logScroll.set_size_request(-1, 300);
+    this->logScroll.set_overlay_scrolling(false);
+    this->logScroll.set_kinetic_scrolling(true);
+    this->mainBox.pack_start(this->logScroll);
+
+    this->progressBar.set_show_text(true);
+    this->progressBar.set_text("0%");
+    this->progressBar.get_style_context()->add_class("progress");
+    this->mainBox.pack_start(this->progressBar, Gtk::PACK_SHRINK);
+
+    this->statusLabel.set_halign(Gtk::ALIGN_START);
+    this->statusLabel.set_markup(
+        "<span style='italic'>Initializing uninstallation...</span>"
+    );
+    this->mainBox.pack_start(this->statusLabel, Gtk::PACK_SHRINK);
+
+    this->closeButton.set_sensitive(false);
+    this->closeButton.get_style_context()->add_class("button");
+    this->closeButton.signal_clicked()
+        .connect(sigc::mem_fun(
+            *this,
+            &UninstallWindow::on_close_clicked
+        ));
+    this->mainBox.pack_start(this->closeButton, Gtk::PACK_SHRINK);
+
+    this->show_all_children();
+    this->workerThread = std::thread(&UninstallWindow::perform_uninstall, this);
+}
+
+UninstallWindow::~UninstallWindow() {
+    if(this->workerThread.joinable())
+        this->workerThread.join();
+}
+
+void UninstallWindow::append_log(const std::string& message) {
+    this->logBuffer->insert(
+        this->logBuffer->end(),
+        message + "\n"
+    );
+}
+
+void UninstallWindow::update_progress(double fraction) {
+    Glib::signal_idle().connect_once([this, fraction]() {
+        progressBar.set_fraction(fraction);
+        progressBar.set_text(Glib::ustring::format(
+            std::fixed, std::setprecision(0), fraction * 100, "%"
+        ));
+    });
+}
+
+void UninstallWindow::update_status(const std::string& text) {
+    Glib::signal_idle().connect_once([this, text]() {
+        statusLabel.set_markup(text);
+    });
+}
+
+void UninstallWindow::on_close_clicked() {
+    this->create_selfdestruct_bat();
+    this->close();
+}
+
+void UninstallWindow::perform_uninstall() {
+    this->append_log("Starting N8 Language uninstallation...");
+    this->update_status("Initializing...");
+    this->update_progress(0.0);
+
+    std::wstring binPath = N8Uninstaller::GetInstallBase(true);
     if(!binPath.empty()) {
-        SetColor(Yellow);
-        std::wcout << L"Removing program files..." << std::endl;
+        this->update_status("Removing program files...");
+        this->append_log("Removing program files...");
 
-        if(DeleteDirectory(binPath)) {
-            SetColor(Green);
-            std::wcout << L"Successfully removed: " << binPath << std::endl;
+        if(N8Uninstaller::DeleteDirectory(binPath)) {
+            this->append_log("Successfully removed program files!");
+            this->update_progress(0.2);
         }
-        else {
-            DWORD err = GetLastError();
-            SetColor(Red);
-
-            std::wcerr << L"Failed to remove: " << binPath 
-                << L" (Error: " << err << L")" << std::endl;
-        }
-
-        ResetColor();
+        else this->append_log("Failed to remove program files!");
     }
 
+    std::wstring docPath = N8Uninstaller::GetInstallBase(false);
     if(!docPath.empty()) {
-        SetColor(Yellow);
-        std::wcout << L"Removing user files..." << std::endl;
-        
-        if(DeleteDirectory(docPath)) {
-            SetColor(Green);
-            std::wcout << L"Successfully removed: " << docPath << std::endl;
+        this->update_status("Removing user files...");
+        this->append_log("Removing user files...");
+
+        if(N8Uninstaller::DeleteDirectory(docPath)) {
+            this->append_log("Successfully removed user files!");
+            this->update_progress(0.4);
         }
-        else {
-            DWORD err = GetLastError();
-            SetColor(Red);
-
-            std::wcerr << L"Failed to remove: " << docPath 
-                << L" (Error: " << err << L")" << std::endl;
-        }
-
-        ResetColor();
+        else this->append_log("Failed to remove user files!");
     }
 
-    SetColor(Yellow);
-    std::wcout << L"Cleaning environment variables..." << std::endl;
-    ResetColor();
+    this->update_status("Cleaning environment...");
+    this->append_log("Removing N8_PATH...");
 
-    if (RemoveEnvironmentVariablePersistent(L"N8_PATH", false)) {
-        SetColor(Green);
-        std::wcout << L"Removed N8_PATH environment variable" << std::endl;
+    if(N8Uninstaller::RemoveEnvironmentVariablePersistent(
+        L"N8_PATH", false
+    )) {
+        this->append_log("Removed N8_PATH environment variable!");
+        this->update_progress(0.6);
     }
-    else {
-        SetColor(Red);
-        std::wcerr << L"Failed to remove N8_PATH" << std::endl;
-    }
-    ResetColor();
+    else this->append_log("Failed to remove N8_PATH!");
 
-    std::wstring binDirPath = GetInstallBase(true) + L"\\bin";
-    if(RemoveFromSystemPath(binDirPath)) {
-        SetColor(Green);
-        std::wcout << L"Removed from PATH: "
-            << binDirPath << std::endl;
-    }
-    else {
-        SetColor(Red);
-        std::wcerr << L"Failed to remove from PATH" << std::endl;
-    }
-    ResetColor();
+    std::wstring binDirPath = N8Uninstaller::GetInstallBase(true) +
+        L"\\bin";
+    this->append_log("Removing from system PATH...");
 
-    SetColor(Yellow);
-    std::wcout << L"Removing registry entries..." << std::endl;
-    ResetColor();
-
-    if(RemoveUninstallEntry()) {
-        SetColor(Green);
-        std::wcout << L"Removed Add/Remove Programs entry" << std::endl;
+    if(N8Uninstaller::RemoveFromSystemPath(binDirPath)) {
+        this->append_log("Removed from system PATH!");
+        this->update_progress(0.7);
     }
-    else {
-        SetColor(Red);
-        std::wcerr << L"Failed to remove uninstall registry entry" << std::endl;
+    else this->append_log("Failed to remove from PATH!");
+
+    this->update_status("Cleaning registry...");
+    this->append_log("Removing uninstall entry...");
+
+    if(N8Uninstaller::RemoveUninstallEntry()) {
+        this->append_log("Removed registry entries!");
+        this->update_progress(0.8);
     }
-    ResetColor();
+    else this->append_log("Failed to remove registry entries!");
 
-    SetColor(Yellow);
-    std::wcout << L"Removing file associations..." << std::endl;
-    ResetColor();
+    this->update_status("Removing file associations...");
+    this->append_log("Removing .n8 associations...");
 
-    if(RemoveFileAssociation()) {
-        SetColor(Green);
-        std::wcout << L"Removed .n8 file associations" << std::endl;
+    if(N8Uninstaller::RemoveFileAssociation()) {
+        this->append_log("Removed file associations!");
+        this->update_progress(0.9);
     }
-    else {
-        SetColor(Red);
-        std::wcerr << L"Failed to remove file associations" << std::endl;
-    }
-    ResetColor();
+    else this->append_log("Failed to remove file associations!");
 
-    SetColor(Aqua);
-    std::wcout << L"\nUninstallation complete!" << std::endl;
-    ResetColor();
+    this->create_selfdestruct_bat();
+    this->update_progress(1.0);
+    this->update_status("Uninstallation complete!");
+    this->append_log("Uninstallation completed successfully!");
 
+    Glib::signal_idle().connect_once([this]() {
+        closeButton.set_sensitive(true);
+    });
+}
+
+void UninstallWindow::create_selfdestruct_bat() {
+    // Get the full path of the current executable.
     wchar_t exePath[MAX_PATH];
-    GetModuleFileNameW(NULL, exePath, MAX_PATH);
+    GetModuleFileNameW(nullptr, exePath, MAX_PATH);
 
+    // Determine the bin directory (the directory containing uninstaller.exe).
+    wchar_t binDir[MAX_PATH];
+    wcscpy_s(binDir, exePath);
+    PathRemoveFileSpecW(binDir);
+
+    // Determine the installation directory by removing the "bin" part.
+    wchar_t installDir[MAX_PATH];
+    wcscpy_s(installDir, binDir);
+    PathRemoveFileSpecW(installDir);
+
+    // Get a temporary directory for creating the batch file.
     wchar_t tempDir[MAX_PATH];
     GetTempPathW(MAX_PATH, tempDir);
 
+    // Create a unique path for the batch file.
     wchar_t batPath[MAX_PATH];
-    srand(static_cast<unsigned int>(time(NULL)));
-    
-    do swprintf_s(
-        batPath,
-        MAX_PATH,
-        L"%s\\n8d_%04x.bat",
-        tempDir,
-        rand() % 0xFFFF
-    );
-    while(PathFileExistsW(batPath));
+    do {
+        swprintf_s(batPath, L"%s\\n8d_%04x.bat", tempDir, rand() % 0xFFFF);
+    } while(PathFileExistsW(batPath));
 
+    // Create the batch file.
     HANDLE hBatFile = CreateFileW(
         batPath,
-        GENERIC_WRITE,
-        0,
-        NULL,
+        GENERIC_WRITE, 0,
+        nullptr,
         CREATE_ALWAYS,
         FILE_ATTRIBUTE_NORMAL,
-        NULL
+        nullptr
     );
 
-    if(hBatFile != INVALID_HANDLE_VALUE) {
-        std::wstring batContent = 
+    if (hBatFile != INVALID_HANDLE_VALUE) {
+        // Build the batch file content (as a wide string) that:
+        // 1. Waits until the uninstaller.exe is deleted.
+        // 2. Recursively deletes the installation directory.
+        // 3. Deletes itself.
+        std::wstring batContentW =
             L"@echo off\r\n"
-            L"timeout /t 1 /nobreak >nul\r\n"
-            L"del \"" + std::wstring(exePath) + L"\"\r\n"
-            L"del \"%~f0\"\r\n";
-        
-        DWORD bytesWritten;
+            L":retry_exe\r\n"
+            L"del \"" + std::wstring(exePath) + L"\" >nul 2>&1\r\n"
+            L"if exist \"" + std::wstring(exePath) + L"\" (\r\n"
+            L"    timeout /t 1 /nobreak >nul\r\n"
+            L"    goto retry_exe\r\n"
+            L")\r\n"
+            L":retry_install\r\n"
+            L"rmdir /S /Q \"" + std::wstring(installDir) + L"\"\r\n"
+            L"if exist \"" + std::wstring(installDir) + L"\" (\r\n"
+            L"    timeout /t 1 /nobreak >nul\r\n"
+            L"    goto retry_install\r\n"
+            L")\r\n"
+            L"del \"%~f0\" >nul 2>&1\r\n";
+
+        // Convert the wide string to an ANSI string so that cmd.exe can interpret it.
+        int ansiSize = WideCharToMultiByte(CP_ACP, 0, batContentW.c_str(), -1, nullptr, 0, nullptr, nullptr);
+        std::string batContentA(ansiSize, '\0');
+        WideCharToMultiByte(CP_ACP, 0, batContentW.c_str(), -1, &batContentA[0], ansiSize, nullptr, nullptr);
+
+        DWORD written;
         WriteFile(
             hBatFile,
-            batContent.c_str(),
-            static_cast<DWORD>(batContent.size() * sizeof(wchar_t)),
-            &bytesWritten,
-            NULL
+            batContentA.c_str(),
+            static_cast<DWORD>(batContentA.size()),
+            &written,
+            nullptr
         );
         CloseHandle(hBatFile);
 
+        // Set up startup info for launching the batch file.
         STARTUPINFOW si = { sizeof(si) };
         PROCESS_INFORMATION pi;
         si.dwFlags = STARTF_USESHOWWINDOW;
         si.wShowWindow = SW_HIDE;
 
+        // Prepare the command line for cmd.exe to execute the batch file.
         std::wstring cmdLine = L"cmd.exe /C \"" + std::wstring(batPath) + L"\"";
-        if(CreateProcessW(
-            NULL,
+
+        // Launch the batch file with its working directory set to the temporary folder
+        // (to avoid locking the installation directory).
+        CreateProcessW(
+            nullptr,
             &cmdLine[0],
-            NULL,
-            NULL,
+            nullptr,
+            nullptr,
             FALSE,
             CREATE_NO_WINDOW,
-            NULL,
-            NULL,
+            nullptr,
+            tempDir,  // current directory is tempDir
             &si,
             &pi
-        )) {
-            CloseHandle(pi.hProcess);
-            CloseHandle(pi.hThread);
-        }
-        else {
-            SetColor(Red);
-            std::wcerr << L"Failed to self-destruct" << std::endl;
-            ResetColor();
-        }
+        );
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if(!N8Uninstaller::IsElevated()) {
+        wchar_t path[MAX_PATH];
+
+        GetModuleFileNameW(nullptr, path, MAX_PATH);
+        ShellExecuteW(nullptr, L"runas", path, nullptr, nullptr, SW_SHOWNORMAL);
+
+        return 0;
     }
 
-    getchar();
-    return 0;
+    auto app = Gtk::Application::create(
+        argc,
+        argv,
+        "org.n8lang.uninstaller"
+    );
+    UninstallWindow window;
+
+    return app->run(window);
 }

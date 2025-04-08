17e987f6c60cdbd1f74b649684d7d301cd3cc01a
Nathanne Isip <nathanneisip@gmail.com>
2025-04-08 08:26:05 +0800

    Updated N8 to Rhea in *.deb installer builder.

--- tools/build_deb_installer.py
+++ tools/build_deb_installer.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2024 - Nathanne Isip
-# This file is part of N8.
+# This file is part of Rhea.
 # 
-# N8 is free software: you can redistribute it and/or modify
+# Rhea is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License,
 # or (at your option) any later version.
 # 
-# N8 is distributed in the hope that it will be useful, but
+# Rhea is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with N8. If not, see <https://www.gnu.org/licenses/>.
+# along with Rhea. If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import platform
@@ -29,10 +29,10 @@ arch_map = {
 
 machine_arch = platform.machine()
 
-PROJECT_NAME    = 'n8-lang'
+PROJECT_NAME    = 'rhea-lang'
 VERSION         = '1.0.0'
 MAINTAINER      = 'Nathanne Isip <nathanneisip@gmail.com>'
-DESCRIPTION     = 'N8 programming language runtime and modules.'
+DESCRIPTION     = 'Rhea programming language runtime and modules.'
 INSTALL_DIR     = f'opt/{PROJECT_NAME}'
 DEB_DIR         = os.path.join('dist', 'deb')
 ARCHITECTURE    = arch_map.get(machine_arch, 'all')
@@ -52,7 +52,7 @@ try:
     create_directory(os.path.join(DEB_DIR, 'etc/fish/conf.d'))
 
     shutil.copytree(
-        os.path.join('dist', 'n8lang'),
+        os.path.join('dist', 'rhea-lang'),
         os.path.join(DEB_DIR, INSTALL_DIR),
         dirs_exist_ok=True
     )
@@ -62,25 +62,25 @@ except Exception as e:
     exit(1)
 
 posix_script_content = f'''#!/bin/sh
-export N8_PATH="/opt/{PROJECT_NAME}"
+export RHEA_PATH="/opt/{PROJECT_NAME}"
 '''
 
-with open(os.path.join(DEB_DIR, 'etc/profile.d/n8lang.sh'), 'w') as f:
+with open(os.path.join(DEB_DIR, 'etc/profile.d/rhea-lang.sh'), 'w') as f:
     f.write(posix_script_content)
-os.chmod(os.path.join(DEB_DIR, 'etc/profile.d/n8lang.sh'), 0o755)
+os.chmod(os.path.join(DEB_DIR, 'etc/profile.d/rhea-lang.sh'), 0o755)
 
 fish_script_content = f'''#!/usr/bin/fish
-set -gx N8_PATH /opt/{PROJECT_NAME}
+set -gx RHEA_PATH /opt/{PROJECT_NAME}
 '''
 
-with open(os.path.join(DEB_DIR, 'etc/fish/conf.d/n8lang.fish'), 'w') as f:
+with open(os.path.join(DEB_DIR, 'etc/fish/conf.d/rhea-lang.fish'), 'w') as f:
     f.write(fish_script_content)
-os.chmod(os.path.join(DEB_DIR, 'etc/fish/conf.d/n8lang.fish'), 0o755)
+os.chmod(os.path.join(DEB_DIR, 'etc/fish/conf.d/rhea-lang.fish'), 0o755)
 
 postinst_content = '''
 #!/bin/sh
 set -e
-export N8_PATH="/opt/n8-lang"
+export RHEA_PATH="/opt/rhea-lang"
 
 add_source_command() {
     local file="$1"
@@ -94,20 +94,20 @@ add_source_command() {
     fi
 }
 
-add_source_command "/etc/bash.bashrc" "source /etc/profile.d/n8lang.sh"
-add_source_command "/etc/zsh/zshenv" "source /etc/profile.d/n8lang.sh"
+add_source_command "/etc/bash.bashrc" "source /etc/profile.d/rhea-lang.sh"
+add_source_command "/etc/zsh/zshenv" "source /etc/profile.d/rhea-lang.sh"
 
 if [ -f "/etc/profile" ]; then
-    add_source_command "/etc/profile" "source /etc/profile.d/n8lang.sh"
+    add_source_command "/etc/profile" "source /etc/profile.d/rhea-lang.sh"
 fi
 
-chmod 755 /etc/profile.d/n8lang.sh
-chmod 755 /etc/fish/conf.d/n8lang.fish
+chmod 755 /etc/profile.d/rhea-lang.sh
+chmod 755 /etc/fish/conf.d/rhea-lang.fish
 
-ln -s /opt/n8-lang/bin/n8 /usr/local/bin/n8
-chmod 755 /usr/local/bin/n8
+ln -s /opt/rhea-lang/bin/rhea /usr/local/bin/rhea
+chmod 755 /usr/local/bin/rhea
 
-echo "N8 Language has been installed successfully!"
+echo "Rhea Language has been installed successfully!"
 exit 0
 '''
 
@@ -118,12 +118,12 @@ os.chmod(postinst_path, 0o755)
 
 postrm_content = '''
 #!/bin/sh
-sed -i '/source \\/etc\\/profile.d\\/n8lang.sh/d' /etc/zsh/zshenv
-sed -i '/source \\/etc\\/profile.d\\/n8lang.sh/d' /etc/bash.bashrc
-sed -i '/source \\/etc\\/profile.d\\/n8lang.sh/d' /etc/profile
+sed -i '/source \\/etc\\/profile.d\\/rhea-lang.sh/d' /etc/zsh/zshenv
+sed -i '/source \\/etc\\/profile.d\\/rhea-lang.sh/d' /etc/bash.bashrc
+sed -i '/source \\/etc\\/profile.d\\/rhea-lang.sh/d' /etc/profile
 
-rm -f /usr/local/bin/n8
-rm -rf /opt/n8-lang
+rm -f /usr/local/bin/rhea
+rm -rf /opt/rhea-lang
 
 exit 0
 '''

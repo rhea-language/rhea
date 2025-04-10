1f34b407f28876e4839f9b4f34ca4778ccf27a74
Nathanne Isip <nathanneisip@gmail.com>
2025-04-11 06:04:05 +0800

    Basic zip archive function implementations on standard library.

---
+++ std/rhea-std/Archive.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of Rhea.
+ * 
+ * Rhea is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * Rhea is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef RHEA_STDLIB_ARCHIVE_CC
+#define RHEA_STDLIB_ARCHIVE_CC
+
+#ifdef __clang__
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+#endif
+
+#include "RheaLibrary.hpp"
+
+RHEA_LIB_START
+
+RHEA_FUNC(archive_zip_open);
+RHEA_FUNC(archive_zip_close);
+
+RHEA_FUNC(archive_zip_compressionMethod);
+
+RHEA_FUNC(archive_zip_setPassword);
+RHEA_FUNC(archive_zip_setEncryption);
+
+RHEA_FUNC(archive_zip_addFromFile);
+RHEA_FUNC(archive_zip_addFromData);
+RHEA_FUNC(archive_zip_addFromDir);
+RHEA_FUNC(archive_zip_addFromString);
+RHEA_FUNC(archive_zip_deleteFile);
+RHEA_FUNC(archive_zip_deleteDir);
+
+RHEA_LIB_END
+
+#ifdef __clang__
+#   pragma clang diagnostic pop
+#endif
+
+#endif

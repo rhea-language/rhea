6d062a8e9201fd567362d65f24c87fae2c6861d3
Nathanne Isip <nathanneisip@gmail.com>
2025-04-17 23:47:44 +0800

    Complete implementation of 'conv' module functions in standard library.

---
+++ std/rhea-std/Conv.hpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025 - Nathanne Isip
+ * This file is part of Rhea.
+ * 
+ * Rhea is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * Rhea is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef RHEA_STDLIB_CONV_CC
+#define RHEA_STDLIB_CONV_CC
+
+#ifdef __clang__
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+#endif
+
+#include "RheaLibrary.hpp"
+
+RHEA_LIB_START
+
+RHEA_FUNC(conv_toString);
+RHEA_FUNC(conv_toNumber);
+RHEA_FUNC(conv_toRegex);
+RHEA_FUNC(conv_toBool);
+
+RHEA_LIB_END
+
+#ifdef __clang__
+#   pragma clang diagnostic pop
+#endif
+
+#endif

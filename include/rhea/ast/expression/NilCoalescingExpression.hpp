22f9c7dde18ab2c91d7c17bf536c75758494d095
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:01:54 +0800

    Standardize formatting for nil coalescing expression header

--- include/rhea/ast/expression/NilCoalescingExpression.hpp
+++ include/rhea/ast/expression/NilCoalescingExpression.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -23,17 +23,15 @@
 #include <rhea/ast/TerminativeSignal.hpp>
 
 class NilCoalescingExpression final : public ASTNode {
-private:
+   private:
     std::shared_ptr<ASTNode> left;
     std::shared_ptr<ASTNode> right;
 
-public:
-    explicit NilCoalescingExpression(
-        std::shared_ptr<Token> _address,
-        std::shared_ptr<ASTNode> _left,
-        std::shared_ptr<ASTNode> _right
-    ) : left(std::move(_left)),
-        right(std::move(_right)) {
+   public:
+    explicit NilCoalescingExpression(std::shared_ptr<Token> _address,
+                                     std::shared_ptr<ASTNode> _left,
+                                     std::shared_ptr<ASTNode> _right)
+        : left(std::move(_left)), right(std::move(_right)) {
         this->address = std::move(_address);
     }
 

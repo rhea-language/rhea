b8db8c6dc5b3ed06e30d58319de73bb33b134385
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:02:14 +0800

    Standardize formatting for standard library mapping definitions

--- std/RheaStdlibMaps.hpp
+++ std/RheaStdlibMaps.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2025 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,12 +19,12 @@
 #ifndef RHEA_STDLIB_MAPS_HPP
 #define RHEA_STDLIB_MAPS_HPP
 
-#include <string>
-#include <unordered_map>
+#include <zip.h>
 
 #include <chisei/neural_network.hpp>
 #include <myshell.hpp>
-#include <zip.h>
+#include <string>
+#include <unordered_map>
 
 #ifndef __TERMUX__
 
@@ -41,10 +41,8 @@ static std::unordered_map<std::string, GLFWcursor*> cursorMap;
 static std::unordered_map<std::string, zip_t*> zipMap;
 
 // ML
-static std::unordered_map<
-    std::string,
-    std::shared_ptr<chisei::NeuralNetwork>
-> neuralNetworkMap;
+static std::unordered_map<std::string, std::shared_ptr<chisei::NeuralNetwork>>
+    neuralNetworkMap;
 
 // Sys
 static std::unordered_map<std::string, std::shared_ptr<MyShell>> shellMap;

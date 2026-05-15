17aaf6ea50c14a1010a298456d10648ff2a879d6
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 17:57:39 +0800

    Improve interpreter shutdown logic to ensure proper resource reclamation

--- src/rhea/ast/statement/HaltStatement.cpp
+++ src/rhea/ast/statement/HaltStatement.cpp
@@ -1,26 +1,27 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <cstdlib>
 #include <rhea/ast/statement/HaltStatement.hpp>
+#include <rhea/core/Runtime.hpp>
 
-DynamicObject HaltStatement::visit(
-    SymbolTable& symbols
-    __attribute__((unused))
-) {
-    exit(0);
+DynamicObject HaltStatement::visit(SymbolTable& symbols) {
+    symbols.waitForTasks();
+    Runtime::cleanUp();
+    std::exit(0);
 }

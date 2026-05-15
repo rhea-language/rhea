404af3b98c80bdce2564fe8d254ddd649ca5b63c
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:33 +0800

    Standardize formatting for string literal expression header

--- include/rhea/ast/expression/StringLiteralExpression.hpp
+++ include/rhea/ast/expression/StringLiteralExpression.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -22,18 +22,16 @@
 #include <rhea/ast/ASTNode.hpp>
 #include <rhea/core/DynamicObject.hpp>
 #include <rhea/core/SymbolTable.hpp>
-
 #include <string>
 
 class StringLiteralExpression final : public ASTNode {
-private:
+   private:
     std::string value;
 
-public:
-    explicit StringLiteralExpression(
-        std::shared_ptr<Token> _address,
-        std::string _value
-    ) : value(_value) {
+   public:
+    explicit StringLiteralExpression(std::shared_ptr<Token> _address,
+                                     std::string _value)
+        : value(_value) {
         this->address = std::move(_address);
     }
 

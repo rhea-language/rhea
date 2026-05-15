73f67c6d098d786ae4ce2759ef1fb2f004fc8761
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:01 +0800

    Standardize formatting for loop expression implementation

--- src/rhea/ast/expression/LoopExpression.cpp
+++ src/rhea/ast/expression/LoopExpression.cpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,11 +25,9 @@ DynamicObject LoopExpression::visit(SymbolTable& symbols) {
     while(this->condition->visit(symbols).booleanEquivalent()) {
         try {
             value = this->body->visit(symbols);
-        }
-        catch(const TerminativeBreakSignal& breakSig) {
+        } catch(const TerminativeBreakSignal& breakSig) {
             break;
-        }
-        catch(const TerminativeContinueSignal& continueSig) {
+        } catch(const TerminativeContinueSignal& continueSig) {
             this->postexpr->visit(symbols);
             continue;
         }

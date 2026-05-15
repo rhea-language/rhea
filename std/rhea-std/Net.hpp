652ae0d3579e6913f6fa197b1722e9d819aa8387
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:02:15 +0800

    Standardize formatting for network native module header

--- std/rhea-std/Net.hpp
+++ std/rhea-std/Net.hpp
@@ -1,27 +1,27 @@
 /*
  * Copyright (c) 2025 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef RHEA_STDLIB_NET_CC
 #define RHEA_STDLIB_NET_CC
- 
+
 #ifdef __clang__
-#   pragma clang diagnostic push
-#   pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
 #endif
 
 #include "RheaLibrary.hpp"
@@ -51,7 +51,7 @@ RHEA_FUNC(net_smtp_sendMailHtml);
 RHEA_LIB_END
 
 #ifdef __clang__
-#   pragma clang diagnostic pop
+#pragma clang diagnostic pop
 #endif
 
 #endif

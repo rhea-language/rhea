7c3b1bd1466a06c5098b3d727620b58528722beb
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:33 +0800

    Standardize formatting for delete statement header

--- include/rhea/ast/statement/DeleteStatement.hpp
+++ include/rhea/ast/statement/DeleteStatement.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -26,14 +26,13 @@
 #include <rhea/parser/Token.hpp>
 
 class DeleteStatement final : public ASTNode {
-private:
+   private:
     std::vector<std::shared_ptr<Token>> variables;
 
-public:
-    explicit DeleteStatement(
-        std::shared_ptr<Token> _address,
-        std::vector<std::shared_ptr<Token>> _variables
-    ) : variables(std::move(_variables)) {
+   public:
+    explicit DeleteStatement(std::shared_ptr<Token> _address,
+                             std::vector<std::shared_ptr<Token>> _variables)
+        : variables(std::move(_variables)) {
         this->address = std::move(_address);
     }
 

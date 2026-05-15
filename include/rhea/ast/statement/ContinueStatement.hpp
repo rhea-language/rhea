6756d434053c2ca6f4cf90d5b2b9b8bfa249278b
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:33 +0800

    Standardize formatting for continue statement header

--- include/rhea/ast/statement/ContinueStatement.hpp
+++ include/rhea/ast/statement/ContinueStatement.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -26,7 +26,7 @@
 #include <rhea/parser/Token.hpp>
 
 class ContinueStatement final : public ASTNode {
-public:
+   public:
     explicit ContinueStatement(std::shared_ptr<Token> _address) {
         this->address = std::move(_address);
     }

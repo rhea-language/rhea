4ae99d43088a6f1ba3b991f86bc1e061d3e14bf8
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:01:54 +0800

    Standardize formatting for block expression header

--- include/rhea/ast/expression/BlockExpression.hpp
+++ include/rhea/ast/expression/BlockExpression.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,21 +19,19 @@
 #ifndef RHEA_AST_EXPR_BLOCK_HPP
 #define RHEA_AST_EXPR_BLOCK_HPP
 
+#include <memory>
 #include <rhea/ast/ASTNode.hpp>
 #include <rhea/ast/TerminativeSignal.hpp>
-
-#include <memory>
 #include <vector>
 
 class BlockExpression final : public ASTNode {
-private:
+   private:
     std::vector<std::shared_ptr<ASTNode>> statements;
 
-public:
-    explicit BlockExpression(
-        std::shared_ptr<Token> _address,
-        std::vector<std::shared_ptr<ASTNode>> _statements
-    ) : statements(std::move(_statements)) {
+   public:
+    explicit BlockExpression(std::shared_ptr<Token> _address,
+                             std::vector<std::shared_ptr<ASTNode>> _statements)
+        : statements(std::move(_statements)) {
         this->address = std::move(_address);
     }
 

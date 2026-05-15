ca7302d339d297dbc04523c944af8e95a5dd1bb3
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:01:54 +0800

    Standardize formatting for binary operator expression header

--- include/rhea/ast/expression/BinaryExpression.hpp
+++ include/rhea/ast/expression/BinaryExpression.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,27 +19,22 @@
 #ifndef RHEA_AST_EXPR_BINARY_HPP
 #define RHEA_AST_EXPR_BINARY_HPP
 
+#include <cmath>
 #include <rhea/ast/ASTNode.hpp>
 #include <rhea/core/DynamicObject.hpp>
 #include <rhea/core/SymbolTable.hpp>
 
-#include <cmath>
-
 class BinaryExpression final : public ASTNode {
-private:
+   private:
     std::shared_ptr<ASTNode> left;
     std::shared_ptr<ASTNode> right;
     std::string op;
 
-public:
-    explicit BinaryExpression(
-        std::shared_ptr<Token> _address,
-        std::shared_ptr<ASTNode> _left,
-        std::string _op,
-        std::shared_ptr<ASTNode> _right
-    ) : left(std::move(_left)),
-        right(std::move(_right)),
-        op(_op) {
+   public:
+    explicit BinaryExpression(std::shared_ptr<Token> _address,
+                              std::shared_ptr<ASTNode> _left, std::string _op,
+                              std::shared_ptr<ASTNode> _right)
+        : left(std::move(_left)), right(std::move(_right)), op(_op) {
         this->address = std::move(_address);
     }
 

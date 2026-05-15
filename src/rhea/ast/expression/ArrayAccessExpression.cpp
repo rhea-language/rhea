819834cee750a3168b4a7909fd2a94e736f439f9
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 17:57:39 +0800

    Add bounds checking and negativity guards for array and string indexing

--- src/rhea/ast/expression/ArrayAccessExpression.cpp
+++ src/rhea/ast/expression/ArrayAccessExpression.cpp
@@ -1,26 +1,25 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <memory>
 #include <rhea/ast/ASTNodeException.hpp>
 #include <rhea/ast/expression/ArrayAccessExpression.hpp>
 #include <rhea/parser/Token.hpp>
-
-#include <memory>
 
 ASTNode* ArrayAccessExpression::getArrayExpression() const {
     return std::move(this->array.get());
@@ -35,33 +34,51 @@ DynamicObject ArrayAccessExpression::visit(SymbolTable& symbols) {
     if(!origin.isArray() && !origin.isString())
         throw ASTNodeException(
             std::move(this->address),
-            "Accessing non-array and non-string object is invalid."
-        );
+            "Accessing non-array and non-string object is invalid.");
 
     DynamicObject idx = this->index->visit(symbols);
     if(origin.isString()) {
         if(!idx.isNumber())
             throw ASTNodeException(
                 std::move(this->address),
-                "Accessing string with non-number index is not allowed."
-            );
+                "Accessing string with non-number index is not allowed.");
 
-        return DynamicObject(
-            std::string(1, origin.getString().at((size_t) idx.getNumber()))
-        );
-    }
-    else if(origin.isArray()) {
+        double rawIdx = idx.getNumber();
+        if(rawIdx < 0)
+            throw ASTNodeException(std::move(this->address),
+                                   "String index cannot be negative.");
+
+        size_t i = static_cast<size_t>(rawIdx);
+        const std::string& str = origin.getString();
+        if(i >= str.size())
+            throw ASTNodeException(std::move(this->address),
+                                   "String index " + std::to_string(i) +
+                                       " is out of bounds (size=" +
+                                       std::to_string(str.size()) + ").");
+
+        return DynamicObject(std::string(1, str[i]));
+    } else if(origin.isArray()) {
         if(!idx.isNumber())
             throw ASTNodeException(
                 std::move(this->address),
-                "Accessing array with non-number index is not allowed."
-            );
+                "Accessing array with non-number index is not allowed.");
 
-        return DynamicObject(std::move(origin.getArray()->at((size_t) idx.getNumber())));
+        double rawIdx = idx.getNumber();
+        if(rawIdx < 0)
+            throw ASTNodeException(std::move(this->address),
+                                   "Array index cannot be negative.");
+
+        size_t i = static_cast<size_t>(rawIdx);
+        auto arr = origin.getArray();
+        if(i >= arr->size())
+            throw ASTNodeException(std::move(this->address),
+                                   "Array index " + std::to_string(i) +
+                                       " is out of bounds (size=" +
+                                       std::to_string(arr->size()) + ").");
+
+        return DynamicObject(std::move(arr->at(i)));
     }
 
-    throw ASTNodeException(
-        std::move(this->address),
-        "Dynamic object is not of array type."
-    );
+    throw ASTNodeException(std::move(this->address),
+                           "Dynamic object is not of array type.");
 }

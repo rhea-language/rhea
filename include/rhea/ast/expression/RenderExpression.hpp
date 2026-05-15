58bbee500fc4bccc48609f1ea7f3a497180cec8f
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:33 +0800

    Standardize formatting for render expression header

--- include/rhea/ast/expression/RenderExpression.hpp
+++ include/rhea/ast/expression/RenderExpression.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -22,20 +22,18 @@
 #include <rhea/ast/ASTNode.hpp>
 
 class RenderExpression final : public ASTNode {
-private:
+   private:
     std::shared_ptr<ASTNode> expression;
     bool newLine;
     bool errorStream;
 
-public:
-    explicit RenderExpression(
-        std::shared_ptr<Token> _address,
-        bool _newLine,
-        bool _errorStream,
-        std::shared_ptr<ASTNode> _expression
-    ) : expression(std::move(_expression)),
-        newLine(_newLine),
-        errorStream(_errorStream) {
+   public:
+    explicit RenderExpression(std::shared_ptr<Token> _address, bool _newLine,
+                              bool _errorStream,
+                              std::shared_ptr<ASTNode> _expression)
+        : expression(std::move(_expression)),
+          newLine(_newLine),
+          errorStream(_errorStream) {
         this->address = std::move(_address);
     }
 

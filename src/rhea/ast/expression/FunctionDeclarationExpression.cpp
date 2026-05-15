1e2cc756ea7a30538a0dd5f0fcdce45ca746493b
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:01 +0800

    Standardize formatting for function declaration implementation

--- src/rhea/ast/expression/FunctionDeclarationExpression.cpp
+++ src/rhea/ast/expression/FunctionDeclarationExpression.cpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -23,8 +23,7 @@
 #include <rhea/parser/Token.hpp>
 
 FunctionDeclarationExpression& FunctionDeclarationExpression::operator=(
-    FunctionDeclarationExpression&& other
-) noexcept {
+    FunctionDeclarationExpression&& other) noexcept {
     if(this != &other) {
         this->address = std::move(other.address);
         this->parameters = std::move(other.parameters);
@@ -34,15 +33,10 @@ FunctionDeclarationExpression& FunctionDeclarationExpression::operator=(
     return *this;
 }
 
-DynamicObject FunctionDeclarationExpression::visit(
-    SymbolTable& symbols
-    __attribute__((unused))
-) {
+DynamicObject FunctionDeclarationExpression::visit(SymbolTable& symbols
+                                                   __attribute__((unused))) {
     return DynamicObject(
-        std::make_shared<FunctionDeclarationExpression>(
-            std::move(*this)
-        )
-    );
+        std::make_shared<FunctionDeclarationExpression>(std::move(*this)));
 }
 
 Token FunctionDeclarationExpression::getFunctionImage() const {
@@ -50,23 +44,17 @@ Token FunctionDeclarationExpression::getFunctionImage() const {
 }
 
 DynamicObject FunctionDeclarationExpression::call(
-    const SymbolTable& symbols,
-    const std::vector<DynamicObject>& args
-) {
+    const SymbolTable& symbols, const std::vector<DynamicObject>& args) {
     if(args.size() != this->parameters.size())
-        throw ASTNodeException(
-            std::move(this->address),
-            "Argument count mismatch, expecting " +
-                std::to_string(this->parameters.size()) + " but go only " +
-                std::to_string(args.size()) + "."
-        );
+        throw ASTNodeException(std::move(this->address),
+                               "Argument count mismatch, expecting " +
+                                   std::to_string(this->parameters.size()) +
+                                   " but go only " +
+                                   std::to_string(args.size()) + ".");
 
     SymbolTable localSymbols(const_cast<SymbolTable&>(symbols));
     parsync(size_t i = 0; i < args.size(); ++i)
-        localSymbols.setSymbol(
-            this->parameters[i],
-            std::move(args).at(i)
-        );
+        localSymbols.setSymbol(this->parameters[i], std::move(args).at(i));
 
     return this->body->visit(localSymbols);
 }

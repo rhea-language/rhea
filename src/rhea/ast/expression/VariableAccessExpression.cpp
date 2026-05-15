d78c85bc735b34df5b6991f563e3c9062afc210a
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:02 +0800

    Standardize formatting for variable access implementation

--- src/rhea/ast/expression/VariableAccessExpression.cpp
+++ src/rhea/ast/expression/VariableAccessExpression.cpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -23,8 +23,5 @@ Token VariableAccessExpression::getName() const {
 }
 
 DynamicObject VariableAccessExpression::visit(SymbolTable& symbols) {
-    return symbols.getSymbol(
-        std::move(this->address),
-        this->name->getImage()
-    );
+    return symbols.getSymbol(std::move(this->address), this->name->getImage());
 }

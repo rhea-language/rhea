52479fcc5a3b6bd6beeae097135ca1e9db6dab18
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:02 +0800

    Standardize formatting for while loop implementation

--- src/rhea/ast/expression/WhileExpression.cpp
+++ src/rhea/ast/expression/WhileExpression.cpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -21,14 +21,11 @@
 DynamicObject WhileExpression::visit(SymbolTable& symbols) {
     DynamicObject value;
 
-    while(this->expression->visit(symbols).booleanEquivalent())
-        try {
+    while(this->expression->visit(symbols).booleanEquivalent()) try {
             value = this->body->visit(symbols);
-        }
-        catch(const TerminativeBreakSignal& breakSig) {
+        } catch(const TerminativeBreakSignal& breakSig) {
             break;
-        }
-        catch(const TerminativeContinueSignal& continueSig) {
+        } catch(const TerminativeContinueSignal& continueSig) {
             continue;
         }
 

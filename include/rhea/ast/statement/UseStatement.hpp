dc889afddcd340c3b37c6a3216110ca6806b23c3
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:03:33 +0800

    Standardize formatting for library inclusion statement header

--- include/rhea/ast/statement/UseStatement.hpp
+++ include/rhea/ast/statement/UseStatement.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -26,17 +26,16 @@
 #include <rhea/parser/Token.hpp>
 
 class UseStatement final : public ASTNode {
-private:
+   private:
     std::shared_ptr<ASTNode> libraryName;
     std::shared_ptr<ASTNode> libraryVersion;
 
-public:
-    explicit UseStatement(
-        std::shared_ptr<Token> _address,
-        std::shared_ptr<ASTNode> _libraryName,
-        std::shared_ptr<ASTNode> _libraryVersion
-    ) : libraryName(std::move(_libraryName)),
-        libraryVersion(std::move(_libraryVersion)) {
+   public:
+    explicit UseStatement(std::shared_ptr<Token> _address,
+                          std::shared_ptr<ASTNode> _libraryName,
+                          std::shared_ptr<ASTNode> _libraryVersion)
+        : libraryName(std::move(_libraryName)),
+          libraryVersion(std::move(_libraryVersion)) {
         this->address = std::move(_address);
     }
 

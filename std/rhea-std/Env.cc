8f2c11739e32ddf269475fe10d0a50208d4f1a99
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:02:15 +0800

    Standardize formatting for environment native module implementation

--- std/rhea-std/Env.cc
+++ std/rhea-std/Env.cc
@@ -1,63 +1,53 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "rhea-std/Env.hpp"
 
+#include <cstdlib>
 #include <rhea/ast/TerminativeSignal.hpp>
 
-#include <cstdlib>
-
 RHEA_FUNC(env_get) {
+    RHEA_FUNC_REQUIRE_UNSAFE
     if(args.size() != 1)
         throw TerminativeThrowSignal(
             std::move(address),
-            "Expecting 1 argument, got " +
-                std::to_string(args.size())
-        );
+            "Expecting 1 argument, got " + std::to_string(args.size()));
 
     DynamicObject value = args.at(0);
-    return DynamicObject(
-        std::string(std::getenv(
-            value.toString().c_str()
-        ))
-    );
+    const char* envVal = std::getenv(value.toString().c_str());
+    return DynamicObject(envVal ? std::string(envVal) : std::string(""));
 }
 
 RHEA_FUNC(env_set) {
+    RHEA_FUNC_REQUIRE_UNSAFE
     if(args.size() != 2)
         throw TerminativeThrowSignal(
             std::move(address),
-            "Expecting 2 argument, got " +
-                std::to_string(args.size())
-        );
+            "Expecting 2 argument, got " + std::to_string(args.size()));
 
-    DynamicObject key = args.at(0),
-        value = args.at(1);
+    DynamicObject key = args.at(0), value = args.at(1);
 
-    #if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
-    return DynamicObject(_putenv(std::string(
-        key.toString() + "=" + value.toString()
-    ).c_str()) != 0);
-    #else
-    return DynamicObject(setenv(
-        key.toString().c_str(),
-        value.toString().c_str(),
-        1
-    ) != 0);
-    #endif
+#if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
+    return DynamicObject(
+        _putenv(std::string(key.toString() + "=" + value.toString()).c_str()) !=
+        0);
+#else
+    return DynamicObject(
+        setenv(key.toString().c_str(), value.toString().c_str(), 1) != 0);
+#endif
 }

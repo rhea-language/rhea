53f813f5d5475fe01adf19369b226f354fad463e
Nathanne Isip <nathanne@nthnn.dev>
2026-05-15 18:01:54 +0800

    Standardize formatting for main Rhea engine header

--- include/Rhea.hpp
+++ include/Rhea.hpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2024 - Nathanne Isip
  * This file is part of Rhea.
- * 
+ *
  * Rhea is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Rhea is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Rhea. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -30,30 +30,31 @@
 #include <rhea/util/ArgumentParser.hpp>
 
 #ifdef __TERMUX__
-#   define RHEA_BUILD_PLATFORM "termux"
+#define RHEA_BUILD_PLATFORM "termux"
 #elif defined(__linux__)
-#   define RHEA_BUILD_PLATFORM "linux"
+#define RHEA_BUILD_PLATFORM "linux"
 #elif defined(__APPLE__)
-#   define RHEA_BUILD_PLATFORM "darwin"
+#define RHEA_BUILD_PLATFORM "darwin"
 #elif defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
-#   define RHEA_BUILD_PLATFORM "windows"
+#define RHEA_BUILD_PLATFORM "windows"
 #else
-#   define RHEA_BUILD_PLATFORM "unknown"
+#define RHEA_BUILD_PLATFORM "unknown"
 #endif
 
 #ifndef __EMSCRIPTEN__
-#   define RHEA_BUILD_TYPE        "beta"
-#   define RHEA_VERSION           "v1.0.0-" RHEA_BUILD_TYPE "-" RHEA_BUILD_PLATFORM
-#   define RHEA_BUILD_TIME        __TIME__ " " __DATE__
+#define RHEA_BUILD_TYPE "beta"
+#define RHEA_VERSION "v1.0.0-" RHEA_BUILD_TYPE "-" RHEA_BUILD_PLATFORM
+#define RHEA_BUILD_TIME __TIME__ " " __DATE__
 
-#   define RHEA_MAIN_BANNER \
-    "        \u001b[38;5;87m___\r\n       /   \\\r\n    "       \
-    "\u001b[38;5;110m__\u001b[38;5;87m|     |\u001b[0m  "       \
-    "\u001b[1;33mRhea Programming Language\u001b[0m\r\n   "     \
-    "\u001b[38;5;110m/   \u001b[38;5;87m\\___/\u001b[0m "       \
-    "  \u001b[1;33m" RHEA_VERSION "          \u001b[0m\r\n"     \
-    "  \u001b[38;5;110m|     |\u001b[0m      \u001b[1;33m"      \
-    RHEA_BUILD_TIME "\u001b[0m\r\n   \u001b[38;5;110m\\___"     \
+#define RHEA_MAIN_BANNER                                                   \
+    "        \u001b[38;5;87m___\r\n       /   \\\r\n    "                  \
+    "\u001b[38;5;110m__\u001b[38;5;87m|     |\u001b[0m  "                  \
+    "\u001b[1;33mRhea Programming Language\u001b[0m\r\n   "                \
+    "\u001b[38;5;110m/   \u001b[38;5;87m\\___/\u001b[0m "                  \
+    "  \u001b[1;33m" RHEA_VERSION                                          \
+    "          \u001b[0m\r\n"                                              \
+    "  \u001b[38;5;110m|     |\u001b[0m      \u001b[1;33m" RHEA_BUILD_TIME \
+    "\u001b[0m\r\n   \u001b[38;5;110m\\___"                                \
     "/\u001b[0m"
 
 #endif
@@ -61,9 +62,9 @@
 #define RHEA_ENV_PATH_NAME "RHEA_PATH"
 
 #ifdef _WIN32
-#   define FS_FILE_SEPARATOR "\\"
+#define FS_FILE_SEPARATOR "\\"
 #else
-#   define FS_FILE_SEPARATOR "/"
+#define FS_FILE_SEPARATOR "/"
 #endif
 
 #define parsync _Pragma("omp parallel for") for
